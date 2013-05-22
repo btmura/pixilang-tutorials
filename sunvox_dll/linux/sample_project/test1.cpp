f7f6f74d188cf80f2ce21247be0a1778e1ba3812
Brian Muramatsu <btmura@gmail.com>
2013-05-22 00:08:28 -0700

    Add unzipped SunVox DLL contents
    NightRadio said it was fine to include the unzipped contents of the zip
    as long as the license file was also included. Including this into the
    project should make it easier for folks to download the entire
    repository and start playing with it. It saves people from the extra
    step of downloading the DLL and then unzipping it in the right spot.

---
+++ sunvox_dll/linux/sample_project/test1.cpp
@@ -0,0 +1,73 @@
+//
+// * Load SunVox song from file.
+// * Send SunVox events.
+// * Play SunVox song.
+//
+  
+#include <stdio.h>
+#include <unistd.h>
+#include <dlfcn.h>
+#include <signal.h>
+
+#define SUNVOX_MAIN
+#include "../../headers/sunvox.h"
+
+int keep_running = 1;
+void int_handler( int param ) 
+{
+    keep_running = 0;
+}
+
+int main()
+{
+    signal( SIGINT, int_handler );
+    
+    if( sv_load_dll() )
+	return 1;
+    
+    int ver = sv_init( 0, 44100, 2, 0 );
+    if( ver >= 0 )
+    {
+	int major = ( ver >> 16 ) & 255;
+	int minor1 = ( ver >> 8 ) & 255;
+	int minor2 = ( ver ) & 255;
+	printf( "SunVox lib version: %d.%d.%d\n", major, minor1, minor2 );
+	
+	sv_open_slot( 0 );
+	
+	printf( "Loading SunVox song from file...\n" );
+	if( sv_load( 0, "test.sunvox" ) == 0 )
+	    printf( "Loaded.\n" );
+	else
+	    printf( "Load error.\n" );
+	sv_volume( 0, 256 );
+	
+	//Send two events (Note ON):
+	sv_send_event( 0, 0, 64, 128, 7, 0, 0 );
+	sleep( 1 );
+	sv_send_event( 0, 0, 64, 128, 7, 0, 0 );
+	sleep( 1 );
+	
+	sv_play_from_beginning( 0 );
+	
+	while( keep_running )
+	{
+	    printf( "Line counter: %d\n", sv_get_current_line( 0 ) );
+	    sleep( 1 );
+	}
+	
+	sv_stop( 0 );
+	
+	sv_close_slot( 0 );
+	
+	sv_deinit();
+    }
+    else 
+    {
+	printf( "sv_init() error %d\n", ver );
+    }
+    
+    sv_unload_dll();
+    
+    return 0;
+}
---
+++ sunvox_dll/readme.txt
@@ -0,0 +1,12 @@
+headers - SunVox C/C++/Pixilang header files.
+ios - SunVox library for iOS + sample projects.
+linux - SunVox library for Linux + sample projects.
+linux_android - SunVox library for Android + sample projects.
+osx - SunVox library for OSX + sample projects.
+pixilang - example of using SunVox library in Pixilang application.
+songs - SunVox songs for the sample projects.
+windows - SunVox library for Windows + sample projects.
+
+changelog.txt - list of changes.
+license.txt - please read this file before using SunVox library in your applications.
+
